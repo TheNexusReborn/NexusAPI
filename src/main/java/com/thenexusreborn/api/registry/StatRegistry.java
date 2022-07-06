7cb42d1b53d54f33fed2cfeaf7434088bb07863d
Firestar311 <slaterandrew0398@gmail.com>
2022-07-06 14:26:45 -0500

    Added a method in StatRegistry to make it easier to register a stat, doesn't need to supply the info class, the new method takes in the parameters needed to it itself. Also added a placeholder for a change to the new levelling system

--- src/main/java/com/thenexusreborn/api/registry/StatRegistry.java
+++ src/main/java/com/thenexusreborn/api/registry/StatRegistry.java
@@ -1,6 +1,9 @@
 package com.thenexusreborn.api.registry;
 
-import com.thenexusreborn.api.stats.Stat;
+import com.thenexusreborn.api.stats.*;
 
 public class StatRegistry extends Registry<Stat .Info> {
+    public void register(String name, StatType type, Object defaultValue) {
+        register(new Stat.Info(StatHelper.formatStatName(name), type, defaultValue));
+    }
 }
