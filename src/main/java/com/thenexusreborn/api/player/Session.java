8351a67202b8ca53da8b25025edd0ba3ff28d557
Firestar311 <slaterandrew0398@gmail.com>
2023-10-25 22:34:23 -0500

    Made it so that the getTimeOnline method in Session accounts for the current session

--- src/main/java/com/thenexusreborn/api/player/Session.java
+++ src/main/java/com/thenexusreborn/api/player/Session.java
@@ -10,37 +10,42 @@ public class Session {
     private UUID uniqueId;
     private long start, end;
     private int gamesPlayed;
-    
-    private Session() {}
-    
+
+    private Session() {
+    }
+
     public Session(UUID uniqueId) {
         this.uniqueId = uniqueId;
     }
-    
+
     public void setGamesPlayed(int gamesPlayed) {
         this.gamesPlayed = gamesPlayed;
     }
-    
+
     public void start() {
         this.start = System.currentTimeMillis();
     }
-    
+
     public void end() {
         this.end = System.currentTimeMillis();
     }
-    
+
     public long getTimeOnline() {
+        if (this.end == 0) {
+           return System.currentTimeMillis() - this.start;
+        }
+
         return this.end - this.start;
     }
-    
+
     public long getStart() {
         return start;
     }
-    
+
     public long getEnd() {
         return end;
     }
-    
+
     public int getGamesPlayed() {
         return this.gamesPlayed;
     }
