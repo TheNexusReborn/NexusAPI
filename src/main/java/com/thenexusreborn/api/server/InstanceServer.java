43c64c4dc9d412e61ea023d978bf6ff19e4bb853
Firestar311 <slaterandrew0398@gmail.com>
2024-04-03 10:18:03 -0600

    Made the primaryVirtualServer in the InstanceServer class be a Property

--- src/main/java/com/thenexusreborn/api/server/InstanceServer.java
+++ src/main/java/com/thenexusreborn/api/server/InstanceServer.java
@@ -1,12 +1,15 @@
 package com.thenexusreborn.api.server;
 
+import com.stardevllc.beans.property.ObjectProperty;
+
 public abstract non-sealed class InstanceServer extends NexusServer {
 
-    protected VirtualServer primaryVirtualServer;
-    protected ServerRegistry<VirtualServer> childServers = new ServerRegistry<>();
+    protected final ObjectProperty<VirtualServer> primaryVirtualServer;
+    private final ServerRegistry<VirtualServer> childServers = new ServerRegistry<>();
     
     public InstanceServer(String name, String mode, int maxPlayers) {
         super(name, ServerType.INSTANCE, mode, maxPlayers);
+        this.primaryVirtualServer = new ObjectProperty<>(this, "primaryVirtualServer", null);
     }
 
     public ServerRegistry<VirtualServer> getChildServers() {
@@ -14,10 +17,10 @@ public abstract non-sealed class InstanceServer extends NexusServer {
     }
 
     public VirtualServer getPrimaryVirtualServer() {
-        return primaryVirtualServer;
+        return primaryVirtualServer.get();
     }
 
     public void setPrimaryVirtualServer(VirtualServer primaryVirtualServer) {
-        this.primaryVirtualServer = primaryVirtualServer;
+        this.primaryVirtualServer.set(primaryVirtualServer);
     }
 }
