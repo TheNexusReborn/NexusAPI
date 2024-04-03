e8da0023884ee0d32215bcabb02e34485e962d9a
Firestar311 <slaterandrew0398@gmail.com>
2024-04-03 10:30:39 -0600

    Made the ParentServer in VirtualServer a property to allow listening for changes

--- src/main/java/com/thenexusreborn/api/server/VirtualServer.java
+++ src/main/java/com/thenexusreborn/api/server/VirtualServer.java
@@ -1,23 +1,54 @@
 package com.thenexusreborn.api.server;
 
+import com.stardevllc.beans.property.ObjectProperty;
+import com.stardevllc.starlib.observable.collections.set.SetChangeListener;
+
+import java.util.UUID;
+
 public abstract non-sealed class VirtualServer extends NexusServer {
     
-    protected InstanceServer parentServer;
+    protected final ObjectProperty<InstanceServer> parentServer;
     
     public VirtualServer(InstanceServer parent, String name, String mode, int maxPlayers) {
         super(name, ServerType.VIRTUAL, mode, maxPlayers);
-        this.parentServer = parent;
+        this.parentServer = new ObjectProperty<>(this, "parentServer", parent);
+        
+        this.parentServer.addListener((observableValue, oldValue, newValue) -> {
+            if (oldValue != null) {
+                for (UUID player : players) {
+                    oldValue.getPlayers().remove(player);
+                }
+            }
+
+            if (newValue != null) {
+                for (UUID player : players) {
+                    newValue.getPlayers().add(player);
+                }
+            }
+        });
+        
+        this.players.addListener((SetChangeListener<UUID>) change -> {
+            if (parentServer.get() == null) {
+                return;
+            }
+            
+            if (change.wasAdded()) {
+                parentServer.get().getPlayers().add(change.getElementAdded());
+            } else if (change.wasRemoved()) {
+                parentServer.get().getPlayers().remove(change.getElementRemoved());
+            }
+        });
     }
 
     public VirtualServer(String name, String mode, int maxPlayers) {
-        super(name, ServerType.VIRTUAL, mode, maxPlayers);
+        this(null, name, mode, maxPlayers);
     }
 
     public InstanceServer getParentServer() {
-        return parentServer;
+        return parentServer.get();
     }
 
     public void setParentServer(InstanceServer parentServer) {
-        this.parentServer = parentServer;
+        this.parentServer.set(parentServer);
     }
 }
